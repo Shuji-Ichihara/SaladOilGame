75fd1db39a71b973fe8fa4984e26114e4ced93cb
katoukanato <104349883+katoukanato@users.noreply.github.com>
2022-11-25 16:45:38 +0900

    Revert "Revert "Player作成、バグあり。""
    This reverts commit a1c026f61f750743dc9fda86dbf38ecc2614c8ca.

---
+++ SaladOilGameProject/Assets/Member/katou/Scripts/ItemDataBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDataBase", menuName = "CreateItemDataBase")]
+public class ItemDataBase : ScriptableObject
+{
+    public List&lt;Item> Items = new List&lt;Item>();
+}
