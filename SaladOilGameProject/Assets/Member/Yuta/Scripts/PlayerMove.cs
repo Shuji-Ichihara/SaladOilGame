2fc4d7aa3f8a2693f80307d7d731481cef28321a
横山 <96154391+YokoyamaYuta@users.noreply.github.com>
2022-11-30 23:30:22 +0900

    プレイヤーが可能になった事 水習得する事 枯れた畑復活させる事

--- SaladOilGameProject/Assets/Member/Yuta/Scripts/PlayerMove.cs
+++ SaladOilGameProject/Assets/Member/Yuta/Scripts/PlayerMove.cs
@@ -4,41 +4,112 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float speed;
 
-    private float speed = 0.5f;
+    private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
+    public bool Get_Water = false;
+    public bool Set_Water = false;
+    public bool Set_Ground = false;
+    public static bool Revival = false;
+
+    private void Start()
     {
-        Vector2 positon = transform.position;
+        Get_Water = false;
+        Set_Water = false;
+        Set_Ground = false;
+        Revival = false;
 
-        if(Input.GetKey("left"))
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("left"))
         {
-            positon.x -= speed;
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
         }
         else if (Input.GetKey("right"))
         {
-            positon.x += speed;
+            rb.velocity = new Vector2(speed, rb.velocity.y);
         }
         else if (Input.GetKey("up"))
         {
-            positon.y += speed;
+            rb.velocity = new Vector2(rb.velocity.x, speed);
         }
         else if (Input.GetKey("down"))
         {
-            positon.y -= speed;
+            rb.velocity = new Vector2(rb.velocity.x, -speed);
+        }
+        else if (!Input.GetKey("left") && !Input.GetKey("right") && !Input.GetKey("up") && !Input.GetKey("down"))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+
+    void Update()
+    {
+        if (Set_Water == true)
+        {
+            if (Input.GetKey(KeyCode.B))
+            {
+                Debug.Log("êÖÉQÉbÉgÇæÇ∫");
+                Get_Water = true;
+            }
         }
 
-        transform.position = positon;
+        if (Set_Ground == true)
+        {
+            if (Input.GetKey(KeyCode.B) && Get_Water)
+            {
+                Debug.Log("î®Ç…êÖÇÉXÉçÅ[ÉCÉì");
+                Get_Water = false;
+                Revival = true;
+            }
+            else if (Input.GetKey(KeyCode.B) && !Get_Water)
+            {
+                Debug.Log("êÖéùÇ¡Çƒñ≥Ç¢ÇÊ");
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Beans")
+        if (collision.gameObject.tag == "Beans")
         {
-            Debug.Log("èEÇ¡ÇΩ");
+            Debug.Log("ìñÇΩÇ¡ÇΩ");
             GameManger.beans -= 1;
             Destroy(collision.gameObject);
+        }
+    }
+
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Water")
+        {
+            Debug.Log("êÖÇ…êGÇÍÇƒÇ‹Å`Ç∑");
+            Set_Water = true;
+        }
+
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("î®ÇÃé¸ÇËÇæÇÊÅ`");
+            Set_Ground = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Water")
+        {
+            Debug.Log("ó£ÇÍÇΩÇÊ");
+            Set_Water = false;
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("ó£ÇÍÇΩÇÊ");
+            Set_Ground = false;
         }
     }
 }
