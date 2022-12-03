9f6dd563fa01c8e5779bc7d7139678c6f65cf248
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2022-12-03 09:13:41 +0900

    Add ResultTextManager.cs
    PlayerControllerTest の豆の所持数を表示する機能を追加した。

--- SaladOilGameProject/Assets/Member/Ichihara/Scripts/ResultTextManager.cs
+++ SaladOilGameProject/Assets/Member/Ichihara/Scripts/ResultTextManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResultTextManager : MonoBehaviour
 {
+    [SerializeField]
+    private string _scoreName = "";
+    private Text _score = null; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _score = GameObject.Find(_scoreName).GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _score.text = PlayerControllerTest.BeanCount.ToString();
     }
 }
